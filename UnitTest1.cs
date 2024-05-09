30fc3a2033aed93869d615de349a6dfacf9157e0
Hycarus <mirkomarasco08@gmail.com>
2024-05-09 13:53:45 +0200

    init

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace net_calculator_tester;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
